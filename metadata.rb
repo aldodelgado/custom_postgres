e4fc5f9413758708484076569b81608f04487283
Aldo Delgado <aldo@osstek.com>
2015-02-24 15:17:42 -0500

    Remvoed postgres from metadata

--- metadata.rb
+++ metadata.rb
@@ -5,5 +5,3 @@ license "MIT"
 description "A chef recipe to create database and user"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version "0.0.1"
-
-depends 'postgres'
